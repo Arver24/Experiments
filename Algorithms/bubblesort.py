7dcbba03c8e2213b41a4be455a4a1e9acebe098e
Arver24 <49467676+Arver24@users.noreply.github.com>
2024-08-19 19:27:53 +0530

    updated files in algorithms

--- Algorithms/bubblesort.py
+++ Algorithms/bubblesort.py
@@ -1,13 +1,22 @@
 def bubblesort(input):
-    for i in range(1, len(input)):
-        if input[i - 1] > input[i]:
-            temp = input[i - 1]
-            input[i - 1] = input[i]
-            input[i] = temp
-        print(input)
+    sorted = False
+    pass_count = 1
+    while not sorted:
+        print(f"pass no: {pass_count}")
+        any_swapping = False
+        for i in range(1, len(input)):
+            if input[i - 1] > input[i]:
+                temp = input[i]
+                input[i] = input[i - 1]
+                input[i - 1] = temp
+                any_swapping = True
+            print(input)
+        if not any_swapping:
+            sorted = True
+        pass_count += 1
     return input
 
 
 arr = [5, 4, 3, 2, 1]
 out = bubblesort(arr)
-print(out)
+# print(out)
